5188bbca1637d17c4356df873689d69e899666a0
sumi2222 <sumiboby@yahoo.ca>
2018-12-13 15:27:24 -0700

    cm

--- src/main/java/com/galvanize/Kumquat_Republic/Controller.java
+++ src/main/java/com/galvanize/Kumquat_Republic/Controller.java
@@ -4,6 +4,7 @@ package com.galvanize.Kumquat_Republic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ public class Controller {
 objectRepository objectRepository;
 
 
-    @PostMapping("product/brand/{brand}")
+    @GetMapping ("product/brand/{brand}")
     public ResponseEntity<ObjectFrame> byBrand (@PathVariable String brand ) {
 
         ObjectFrame obj = new ObjectFrame();
@@ -26,7 +27,7 @@ objectRepository objectRepository;
         return new ResponseEntity<ObjectFrame>(objectFrame.get(),HttpStatus.OK);
     }
 
-    @PostMapping("product/brand/{color}")
+    @GetMapping("product/brand/{color}")
     public ResponseEntity<ObjectFrame> byColor (@PathVariable String color ) {
 
         ObjectFrame obj = new ObjectFrame();
@@ -34,7 +35,7 @@ objectRepository objectRepository;
         return new ResponseEntity<ObjectFrame>(objectFrame.get(),HttpStatus.OK);
     }
 
-    @PostMapping("product/brand/{price1}/{price2}")
+    @GetMapping("product/brand/{price1}/{price2}")
     public ResponseEntity<ObjectFrame> byprice (@PathVariable Double price1,@PathVariable Double price2 ) {
 
         ObjectFrame obj = new ObjectFrame();
@@ -42,7 +43,7 @@ objectRepository objectRepository;
         return new ResponseEntity<ObjectFrame>(objectFrame.get(),HttpStatus.OK);
     }
 
-    @PostMapping("product/brand/{brand}/{color}")
+    @GetMapping("product/brand/{brand}/{color}")
     public ResponseEntity<ObjectFrame> byfilter (@PathVariable String brand,@PathVariable String color ) {
 
         ObjectFrame obj = new ObjectFrame();
@@ -50,7 +51,7 @@ objectRepository objectRepository;
         return new ResponseEntity<ObjectFrame>(objectFrame.get(),HttpStatus.OK);
     }
 
-    @PostMapping("product/brand/ascprice")
+    @GetMapping("product/brand/ascprice")
     public ResponseEntity<ObjectFrame> Ascprice () {
 
         ObjectFrame obj = new ObjectFrame();
@@ -58,7 +59,7 @@ objectRepository objectRepository;
         return new ResponseEntity<ObjectFrame>(objectFrame.get(),HttpStatus.OK);
     }
 
-    @PostMapping("product/brand/descprice")
+    @GetMapping("product/brand/descprice")
     public ResponseEntity<ObjectFrame> descprice () {
 
         ObjectFrame obj = new ObjectFrame();
